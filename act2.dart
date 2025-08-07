6c5cb54bb5cdf980a76a19f92f57fd55b8e0f238
TENJOU <168693369+tenjou1602@users.noreply.github.com>
2025-08-07 09:49:34 +0000

    act2.dart

---
+++ act2.dart
@@ -0,0 +1,25 @@
+void main() {
+
+double result calculatePerimeter (7, 2, true); 
+print("The result for Square + result.toString());
+	result calculatePerimeter(10, 5, false);
+
+	result.toString()); 12 print("The result for Rectangle")
+
+}
+double calculatePerimeter(double width, double height, bool isSquare) {
+double area;
+if (isSquare) {
+
+area=4 width;
+
+} else {
+
+area2 (width + height);
+
+}
+
+return area;
+
+}
+
